68aa1fa3fb2c147922d5ca98ca51c9376a4466b8
Anthony <Kain235@gmail.com>
2022-06-29 12:31:45 -0400

    commit tree 7344da40980a95420ee47c45cc3abf1592e12c72

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,29 @@
 class ItemsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   def index
+    if(params[:user_id])
+    items = User.find(params[:user_id]).items
+    else
     items = Item.all
+    end
     render json: items, include: :user
   end
 
+  def show
+  item = Item.find(params[:id])
+  render json: item
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(name: params[:name],price: params[:price], description: params[:description])
+    render json: item, status: 201
+  end
+
+  private
+  def render_not_found_response
+    render json: {errors: "Not found"}, status: 404
+  end
+
+  
 end
